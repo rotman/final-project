2fe060a4499f065e5c9e57946fb7db084956b319
Ruben Jami <rubenjami@Rubens-MacBook-Pro.local>
2016-03-31 16:52:05 +0300

    added the preper message

--- middle_layer/middle_layer.ino
+++ middle_layer/middle_layer.ino
@@ -3,6 +3,10 @@
 #include <message.h>
 #include <SPI.h>
 
+#define LOWER_LAYER_ADDRESS 1
+#define UPPER_LAYER_ADDRESS 201
+#define MY_ADDRESS 101
+
 //globals
 //-------
 RF24 radio(7, 8);
@@ -63,117 +67,142 @@ void sendMessage(Message message){
 }
 
 void decodeMessage(Message msg) {
-  //TODO check from which layer the message came from
-
-  //if the meesgae came from high layer
-  //we should change policy in this layer/bottom layer
+  if(msg.source >= 200 && msg.source < 300){   //from higer layer
+      //if the meesgae came from high layer
+      //we should change policy in this layer/bottom layer
+      
+      /*switch(msg->sensorType) {
+      //need to change soil humidity treshold in the bottom layer
+      case 'S':
+      //maybe prepare diiferent message
+      //change msg src and dest
+      //sendMessage(msg);
+      break;
+      case 'T': 
+      //set the temperature treshold in this layer
+      //no need to send down
+      break;
+    
+      case 'H':
+      //set the humidity treshold in this layer
+      //no need to send down 
+      break;
+    
+      case 'L':
+      //set the light treshold in this layer
+      //no need to send down
+      break;
+    
+      }*/
+  }
   
-  /*switch(msg->sensorType) {
-  //need to change soil humidity treshold in the bottom layer
-  case 'S':
-  //maybe prepare diiferent message
-  //change msg src and dest
-  //sendMessage(msg);
-  break;
-  case 'T': 
-  //set the temperature treshold in this layer
-  //no need to send down
-  break;
 
-  case 'H':
-  //set the humidity treshold in this layer
-  //no need to send down 
-  break;
-
-  case 'L':
-  //set the light treshold in this layer
-  //no need to send down
-  break;
-
-  }*/
 
   //if the meesgae came from bottom layer
   //the layer should act/send up the hirarchy if needed
-  switch(msg.sensorType) {
-    
-    //soil humidity data
-    case 'S':
+  if(msg.source >= 1 && msg.source < 100){
+    switch(msg.sensorType) {
+      
+      /******************soil humidity data*******************/
+      case 'S':
+  
+      //do nothing, just send to high level for image status
+      //change msg src and dest
+      //sendMessage(msg)
+      break;
+      
+      /******************temparture data*********************/
+      case 'T':
+      Serial.println("sensorType");
+      Serial.println(msg.sensorType);
+      Serial.println("Destination");
+      Serial.println(msg.dest);
+      Serial.println("Data temperature");
+      Serial.println(msg.data[0], DEC);
+      Serial.println("Data humidity");
+      Serial.println(msg.data[2], DEC);
 
-    //do nothing, just send to high level for image status
-    //change msg src and dest
-    //sendMessage(msg)
-    break;
-    
-    //temparture data
-    case 'T':
-    Serial.println("sensorType");
-    Serial.println(msg.sensorType);
-    Serial.println("Destination");
-    Serial.println(msg.dest);
-        Serial.println("Data temperature");
-    Serial.println(msg.data[0], DEC);
-    Serial.println("Data humidity");
-    Serial.println(msg.data[2], DEC);
-    
-
-
-    //do avreage from all temperatre messages that received
-    //if (average < TEMP_LOWER_TRESHOLD) {
-      //turn on heat lamp
-    //}
-    //else if (average > TEMP_HIGHER_TRESHOLD) {
-      //turn on fan in
-    //}
-    //else {
-      //do nothing, just check that the actuators are off
-    //}
-    //send average to upper layer
-    break;
-    
-    //air humidity data
-    case 'H':
-    //do avreage from all humidity messages that received
-    //if (average < HUMIDITY_LOWER_TRESHOLD) {
-      //check how to add humidity to greenhouse
-    //}
-    //else if (average > HUMIDITY_HIGHER_TRESHOLD) {
-      //turn on fan out
-    //}
-    //else {
-      //do nothing, just check that the actuators are off
-    //}
-    //send average to upper layer
-    break;
-    
-    //light data
-    case 'L':
-    //do avreage from all light messages that received
-    //if (average < LIGHT_LOWER_TRESHOLD && needLight) {
-      //turn on lamp
-    //}
-    //else if (average > LIGHT_HIGHER_TRESHOLD && !needLight) {
-      //turn off lump
-    //}
-    //else {
-      //do nothing, just check that the actuators are off
-    //}
-    //send average to upper layer  
-    break;
-    
-    //water consumption data
-    case 'W':
-    //TODO sum the amount of water and send up
-    //sendMessageToUpperLayer()
-    break;
-    
-    //TODO electricty consumption   
-
-    default:
-    //TODO error
-    break;
+      msg = prepareMessageToLower(msg);
+      msg.data[0] = 35;
+      msg.data[1] = 55;
+      msg.data[2] = 36;
+      msg.data[3] = 56;
+      
+  
+  
+      //do avreage from all temperatre messages that received
+      //if (average < TEMP_LOWER_TRESHOLD) {
+        //turn on heat lamp
+      //}
+      //else if (average > TEMP_HIGHER_TRESHOLD) {
+        //turn on fan in
+      //}
+      //else {
+        //do nothing, just check that the actuators are off
+      //}
+      //send average to upper layer
+      break;
+      
+      /***********************air humidity data***********************/
+      case 'H':
+      //do avreage from all humidity messages that received
+      //if (average < HUMIDITY_LOWER_TRESHOLD) {
+        //check how to add humidity to greenhouse
+      //}
+      //else if (average > HUMIDITY_HIGHER_TRESHOLD) {
+        //turn on fan out
+      //}
+      //else {
+        //do nothing, just check that the actuators are off
+      //}
+      //send average to upper layer
+      break;
+      
+      /***************************light data***********************/
+      case 'L':
+      //do avreage from all light messages that received
+      //if (average < LIGHT_LOWER_TRESHOLD && needLight) {
+        //turn on lamp
+      //}
+      //else if (average > LIGHT_HIGHER_TRESHOLD && !needLight) {
+        //turn off lump
+      //}
+      //else {
+        //do nothing, just check that the actuators are off
+      //}
+      //send average to upper layer  
+      break;
+      
+      /*********************water consumption data*****************/
+      case 'W':
+      //TODO sum the amount of water and send up
+      //sendMessageToUpperLayer()
+      break;
+      
+      //TODO electricty consumption   
+  
+      default:
+      //TODO error
+      break;
+    }
   }
   
 }
+
+Message prepareMessageToUpper(Message message){
+  message.source = MY_ADDRESS;
+  message.dest = UPPER_LAYER_ADDRESS;
+  return message;
+}
+
+
+Message prepareMessageToLower(Message message){
+  message.source = MY_ADDRESS;
+  message.dest = LOWER_LAYER_ADDRESS;
+  return message;
+}
+
+
 
 void loop() {
   //receive message from lower layer
