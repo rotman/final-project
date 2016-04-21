4bfef517534a71741af0504ef1745f7de0d9579f
Rotem Matityahu <rotem.matityahu@ironsrc.com>
2016-04-21 15:28:48 +0300

    soil humidity sensor

---
+++ Arduino folder/libraries/SSoil/SSoil.cpp
@@ -0,0 +1,32 @@
+#include <SSoil.h>
+
+
+
+SSoil::SSoil(int pin){
+	this->pin = pin;
+	sensorValue = 0;
+}
+
+Message SSoil	::readSensorData(){
+	Message message;					//create new message
+
+	Serial.println("readSensorData called");
+	sensorValue = analogRead(pin);
+
+	Serial.print("soil humidity:");
+	Serial.println(sensorValue, DEC);
+
+  message.data[0]= sensorValue;		//enter the data
+  message.sensorType = 'S';
+  Serial.print("copied from sensor to messege: check: ");
+  Serial.println(message.data[0],DEC);	
+ 
+  return message;
+}
+
+
+
+
+
+
+
