a7952bf1db35451ae4090c32b3c69299b0ed06aa
Rotem Matityahu <rotem.matityahu@ironsrc.com>
2016-07-01 15:02:26 +0300

    pretty CommonValues

--- Arduino folder/libraries/CommonValues/CommonValues.h
+++ Arduino folder/libraries/CommonValues/CommonValues.h
@@ -1,8 +1,11 @@
 #ifndef COMMON_VALUES
 #define COMMON_VALUES
+
 #include <Arduino.h>
+
 class CommonValues {
 	public:
+	
 		//addresses
 		static const int lowerLayerMinAddress		 = 1;
 		static const int lowerLayerMaxAddress		 = 99;
@@ -25,48 +28,44 @@ class CommonValues {
 		static const int currentSensorId			 = 5;
 		static const int waterFlowSensorId			 = 6;
 
-
 		//input pins
-		static const int radioPin1 = 7;
-		static const int radioPin2 = 8;
-		static const int tempHumidityPin = 2;
-		static const int soilPin = A0;
-		static const int lightPin = A1;
+		static const int radioPin1 			   = 7;
+		static const int radioPin2 			   = 8;
+		static const int tempHumidityPin 	   = 2;
+		static const int soilPin 			   = A0;
+		static const int lightPin 			   = A1;
 		static const int currentConsumptionPin = A5;
-		static const int waterConsumptionPin1 = 2;
-		static const int waterConsumptionPin2 = 3;
-		static const int higherLayerRadioPin1 = 2;
-		static const int higherLayerRadioPin2 = 15;
+		static const int waterConsumptionPin1  = 2;
+		static const int waterConsumptionPin2  = 3;
+		static const int higherLayerRadioPin1  = 2;
+		static const int higherLayerRadioPin2  = 15;
 
 		//actuators pins
-		static const int pumpPin = 6;
-
-		static const int fanPin = 3;
-		static const int ventPin = 4;
+		static const int pumpPin  = 6;
+		static const int fanPin   = 3;
+		static const int ventPin  = 4;
 		static const int steamPin = 10;
-		static const int lampPin = 9;
-		static const int heatPin = 5;
-		
-
+		static const int lampPin  = 9;
+		static const int heatPin  = 5;
 
 		//Conventions
-		static const char emptyMessage = 'z';
-		static const char policyChange = 'p';
-		static const char loopTimeChange = 'a';
-		static const char myAddressChange = 'b';
-		static const char yourAddressChange = 'y';
+		static const char emptyMessage		 = 'z';
+		static const char policyChange 		 = 'p';
+		static const char loopTimeChange 	 = 'a';
+		static const char myAddressChange	 = 'b';
+		static const char yourAddressChange  = 'y';
 		static const char arduinoMalfunction = 'e';
-		static const char dataType = 'd';
-		static const char emergencyType = 'E';
-		static const char ACTION_TYPE = 'A';
+		static const char dataType			 = 'd';
+		static const char emergencyType		 = 'E';
+		static const char ACTION_TYPE		 = 'A';
 
 		//sensor types
-		static const char temperatureType = 'T';
-		static const char humidityType = 'H';
+		static const char temperatureType  = 'T';
+		static const char humidityType 	   = 'H';
 		static const char soilHumidityType = 'S';
-		static const char lightType = 'L';
-		static const char currentType = 'C';
-		static const char waterType = 'W';
+		static const char lightType		   = 'L';
+		static const char currentType	   = 'C';
+		static const char waterType		   = 'W';
 
 		static const int exponentialBackoffMaxRetries = 7;
 		static const int sendMaxRetries				  = 10;
@@ -75,20 +74,19 @@ class CommonValues {
 		static const int lowerLayerRegisteredNum	  = 2;
 		static const int EMERGENCY_TEMPERATURE		  = 40;
 
-		static const unsigned long day		= 86400000;  //24 hour
-		static const unsigned long hour		= 3600000;
-		static const unsigned long minute	= 60000;
-		static const unsigned long second	= 1000;
+		//time consts
+		static const unsigned long day	  = 86400000;  //24 hour
+		static const unsigned long hour	  = 3600000;
+		static const unsigned long minute = 60000;
+		static const unsigned long second = 1000;
 		
 		//actuators intervals
-		static const unsigned long fanTime = 10 * second;
-		static const unsigned long ventTime = 8 * second;
-		static const unsigned long heatTime = 5 * second;
+		static const unsigned long fanTime   = 10 * second;
+		static const unsigned long ventTime  = 8 * second;
+		static const unsigned long heatTime  = 5 * second;
 		static const unsigned long steamTime = 7 * second;
-		static const unsigned long pumpTime = 3 * second;
-		static const unsigned long lampTime = 3 * second;
-
-
+		static const unsigned long pumpTime  = 3 * second;
+		static const unsigned long lampTime  = 3 * second;
 
 		//thresholds - not consts!
 		static float soilHumidityThresholdMin;
@@ -111,12 +109,13 @@ class CommonValues {
 		static const char* password;
 		static const char* host;
 		static const char* key;
-		static const unsigned long lastTimeSentToServer = 0;
-		static const long sendToServerInterval = 5000;
+		
+		static const unsigned long lastTimeSentToServer 		 = 0;
+		static const long sendToServerInterval					 = 5000;
 		static const unsigned long lastTimeCheckedForNewSettings = 0;
-		static const long checkedForNewSettingsInterval = 10000;
-		static const int amountOfGreenHouses = 1;
-		static const int MiddleLayerLostConnectionTime = 50000;
+		static const long checkedForNewSettingsInterval			 = 10000;
+		static const int amountOfGreenHouses					 = 1;
+		static const int MiddleLayerLostConnectionTime			 = 50000;
 
 };
 
